fb2260d8664ce612e917e6f34178c8d4a28ced3e
Mr-Forder <colforder@gmail.com>
2022-11-08 12:57:49 +0000

    form markup added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        latoRegular: "Lato-Regular",
+        latoBold: "Lato-Bold",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
